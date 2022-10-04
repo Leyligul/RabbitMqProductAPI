09833ade32caf8c59a8c6a01e84a2ba80e92ef3d
DKrkc <92672947+DKrkc@users.noreply.github.com>
2022-10-04 14:57:40 +0300

    Add project files.

---
+++ RabbitMqProductAPI/Context/DbContextProduct.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using RabbitMqProductAPI.Models;
+
+namespace RabbitMqProductAPI.Context
+{
+    public class DbContextProduct: DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public DbContextProduct(IConfiguration configuration) 
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet <Product> Products { get; set; }
+
+    }
+}
+
---
+++ RabbitMqProductAPI/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace RabbitMqProductAPI.Models
+{
+    public class Product
+    {
+
+        public int ProductId { get; set; }
+        public int ProductName{ get; set; }
+        public int ProductDescription { get; set; }
+        public int ProductPrice { get; set; }
+        public int ProductStock { get; set; }
+    }
+}
